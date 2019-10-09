86fc2688b8025cb4aeadb43def3df0acd90307cf
Athelia Crosmun <athelia.hk@gmail.com>
2019-10-09 02:40:07 +0000

    Completed version 2

--- version2/raffle.py
+++ version2/raffle.py
@@ -4,3 +4,12 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
+import customers
+
+def run_raffle():
+    """Run the weekly raffle."""
+
+    customers = get_customers_from_file("customers.txt")
+    pick_winner(customers)
+
+run_raffle()
