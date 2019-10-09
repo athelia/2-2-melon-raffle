bb241f4c14f5d45277c71fb4f2e3422f76759ee2
Athelia Crosmun <athelia.hk@gmail.com>
2019-10-09 02:49:06 +0000

    Actually completed version 2

--- version2/raffle.py
+++ version2/raffle.py
@@ -4,7 +4,20 @@
 
 # Hint: remember to import any functions you need from
 # other files or libraries
-import customers
+
+import random
+
+from customers import get_customers_from_file
+
+def pick_winner(customers):
+    """Choose a random winner from list of customers."""
+
+    chosen_customer = random.choice(customers)
+
+    print("Tell {name} at {email} that they've won".format(
+        name=chosen_customer.name,
+        email=chosen_customer.email
+    ))
 
 def run_raffle():
     """Run the weekly raffle."""
@@ -12,4 +25,5 @@ def run_raffle():
     customers = get_customers_from_file("customers.txt")
     pick_winner(customers)
 
-run_raffle()
+if __name__ == "__main__":
+    run_raffle()
